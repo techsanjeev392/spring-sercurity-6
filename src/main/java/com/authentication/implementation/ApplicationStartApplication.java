f09080cce1fde9b2dbf291b7c61c8e88929e88b5
Sanjeev Kumar <sanjeevf800@gmail.com>
2025-03-28 01:31:48 +0530

    first commit -Initial changes

---
+++ src/main/java/com/authentication/implementation/ApplicationStartApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authentication/implementation/ApplicationStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
